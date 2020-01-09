ff92f90d25af7aa78ebc3051d90c0cefc3a7e9c6
deepak <deepak1992ac@gmail.com>
2020-01-09 15:16:25 +0530

    removed un used constrante

--- src/main/java/com/userAuth/model/User.java
+++ src/main/java/com/userAuth/model/User.java
@@ -8,37 +8,17 @@ import java.util.Set;
 
 @Entity
 @NoArgsConstructor
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = {
-                "username"
-        }),
-        @UniqueConstraint(columnNames = {
-                "email"
-        })
-})
+@Table(name = "users")
+@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Long id;
-
-    @Getter
-    @Setter
     private String userName;
-
-    @Getter
-    @Setter
     private String email;
-
-    @Getter
-    @Setter
     private String password;
-
     @ManyToMany(fetch = FetchType.LAZY)
-    @Getter
-    @Setter
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
